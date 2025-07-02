9ebc52ccedea28cf1a23492846610a305cae3d61
soygeanny <geannycuevaz@gmail.com>
2025-07-02 11:06:06 -0500

    chore:Sprint3

--- Controller/Rutina.java
+++ Controller/Rutina.java
@@ -11,22 +11,4 @@ public class Rutina {
         System.out.println("5. Estiramiento - 5 minutos");
     }
 
-    public void mostrarRutina() {
-        System.out.println("Mostrando la rutina de entrenamiento:");
-        System.out.println("1. Calentamiento - 10 minutos");
-        System.out.println("2. Sentadillas - 3 series de 12 repeticiones");
-        System.out.println("3. Flexiones - 3 series de 10 repeticiones");
-        System.out.println("4. Abdominales - 3 series de 15 repeticiones");
-        System.out.println("5. Estiramiento - 5 minutos");
-    }
-
-    public void mostrarRutina() {
-        System.out.println("Mostrando la rutina de entrenamiento:");
-        System.out.println("1. Calentamiento - 10 minutos");
-        System.out.println("2. Sentadillas - 3 series de 12 repeticiones");
-        System.out.println("3. Flexiones - 3 series de 10 repeticiones");
-        System.out.println("4. Abdominales - 3 series de 15 repeticiones");
-        System.out.println("5. Estiramiento - 5 minutos");
-    }
-
 }
